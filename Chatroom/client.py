c5e1a7997099b871c669334309025c6ac017b6c5
Mahamane Dicko <99940327+Maitre-Mad@users.noreply.github.com>
2023-11-30 22:46:06 -0500

    Update client.py
    adding a few comments

--- Chatroom/client.py
+++ Chatroom/client.py
@@ -1,7 +1,9 @@
 import threading
 import socket
 
+#Creating a socket using IPv4 and tcp
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#We connect to the localhost using the port 55555
 client.connect(("127.0.0.1",55555))
 
 def receive():
